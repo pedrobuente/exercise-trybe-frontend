dd935518219560365148501e3912ce90fc197fa4
Pedro Silva Buente <pedrobuente@gmail.com>
2021-09-03 18:36:48 -0300

    commit inicial

---
+++ componenentes-estados-eventos/fixacao/fixar/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import fixa from './fixacao1';
+
+/* Embora isso funcione, essa DEFINITIVAMENTE
+não é a maneira correta de se criar eventos
+em React! A função se refere ao componente,
+então deve ser parte de sua classe! */
+
+class App extends React.Component {
+  /* Repare que, diferentemente do HTML, no
+  JSX você associa uma função a um evento
+  passando a própria função entre chaves `{}` */
+  render() {
+    return 
+  }
+}
+
+export default App;
