3aa663c30b4ffcdc9e5dc97a3a9feea5417adb12
Pedro Silva Buente <pedrobuente@gmail.com>
2021-09-13 15:07:03 -0300

    fixacao 2

--- fixacao/fixacao-1/src/fixacao1.js
+++ fixacao/fixacao-1/src/fixacao1.js
@@ -1,63 +1,71 @@
 import React from 'react';
 
 class DadJoke extends React.Component {
-    constructor() {
-      super();
-  
-      this.saveJoke = this.saveJoke.bind(this);
-      this.renderJokeElement = this.renderJokeElement.bind(this);
-  
-      this.state = {
-        jokeObj: undefined,
-        loading: true,
-        storedJokes: [],
-      }
+  constructor() {
+    super();
+
+    this.saveJoke = this.saveJoke.bind(this);
+    this.renderJokeElement = this.renderJokeElement.bind(this);
+
+    this.state = {
+      jokeObj: undefined,
+      loading: true,
+      storedJokes: [],
     }
-  
-    async fetchJoke() {
+  }
+
+  async fetchJoke() {
+    this.setState(
+      { loading: true }, // Primeiro parâmetro da setState()!
+      async () => {
       const requestHeaders = { headers: { Accept: 'application/json' } }
       const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
       const requestObject = await requestReturn.json();
       this.setState({
-        jokeObj: requestObject,
-      })
-    }
-  
-    componentDidMount() {
-      this.fetchJoke();
-    }
-  
-    saveJoke() {
-      //Salvando a piada no array de piadas existentes
-  
-    }
-  
-    renderJokeElement() {
-      return (
-        <div>
-          <p>{this.state.jokeObj.joke}</p>
-          <button type="button" onClick={this.saveJoke}>
-            Salvar piada!
-          </button>
-        </div>
-      );
-    }
-  
-    render() {
-      const { storedJokes } = this.state;
-      const loadingElement = <span>Loading...</span>;
-  
-      return (
-        <div>
-          <span>
-            {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
-          </span>
-  
-        <span>RENDERIZAÇÃO CONDICIONAL</span>
-  
-        </div>
-      );
-    }
+        loading: false,
+        jokeObj: requestObject
+      });
+    });
   }
+
+  componentDidMount() {
+    this.fetchJoke();
+  }
+
+  saveJoke() {
+    this.setState(({ storedJokes, jokeObj }) => ({
+      storedJokes: [...storedJokes, jokeObj]
+    }));
+
+    this.fetchJoke();
+  }
+
+  renderJokeElement() {
+    return (
+      <div>
+        <p>{this.state.jokeObj.joke}</p>
+        <button type="button" onClick={this.saveJoke}>
+          Salvar piada!
+        </button>
+      </div>
+    );
+  }
+
+  render() {
+    const { storedJokes, loading } = this.state;
+    const loadingElement = <span>Loading...</span>;
+
+    return (
+      <div>
+        <span>
+          {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
+        </span>
+
+      <p>{loading ? loadingElement : this.renderJokeElement() }</p>
+
+      </div>
+    );
+  }
+}
 
   export default DadJoke;
